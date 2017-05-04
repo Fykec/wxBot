86d86a9e6e7726294a3f8c1a014f6509039bced7
卫占魁 <weizhankui2008@gmail.com>
2017-05-04 18:32:16 +0800

    增加注释、以及增加新加群之后重新获取联系人信息

--- newsbot.py
+++ newsbot.py
@@ -20,11 +20,16 @@ SCHEDULE_MINUTE = 0
 class NewsWXBot(WXBot):
 
     def handle_msg_all(self, msg):
+        #群昵称，进群之后，必须改为这个昵称
         self.nickName = u'小猫助手'
         reload(sys) 
         sys.setdefaultencoding('utf8')
         ##群文本消息
         if msg['msg_type_id'] == 3 and msg['content']['type'] == 0:
+            #群名是unknown，重新获取一遍联系人
+            if msg['user']['name'] == 'unknown' or msg['content']['user']['name'] == 'unknown':
+                print 'get contact again'
+                self.get_contact()
             #@了
             if msg['content']['data'].find('@'+self.nickName) != -1:
                 if msg['content']['data'].find(u'论坛') != -1:
